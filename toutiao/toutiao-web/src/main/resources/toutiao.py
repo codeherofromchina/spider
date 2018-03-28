0fa7102a076d354f59779764611fd2afdc91ca2d
王晓丹 <wangxiaodan@163.com>
2018-03-28 22:54:37 +0800

    修正python文件

--- toutiao/toutiao-web/src/main/resources/toutiao.py
+++ toutiao/toutiao-web/src/main/resources/toutiao.py
@@ -3,9 +3,10 @@
 from redis import Redis
 from selenium import webdriver
 
+print('准备python服务器')
 ##打开浏览器
 firefox = webdriver.Firefox()
-firefox.get('https://www.toutiao.com')
+firefox.get('https://www.toutiao.com/ch/news_image/')
 ##获取cookie
 dictCookies = firefox.get_cookies()
 cookieHeader = ''
@@ -31,8 +32,10 @@ subChannel = 'toutiaopush'
 pub = conn.pubsub()
 pub.subscribe(subChannel)
 pub.parse_response()
+print('python服务器准备完成')
 while True:
 	##获取签名并发送信息
-	max_behot_time= redis_sub.parse_response()
+	max_behot_time= pub.parse_response()
+	print('max_behot_time:' + str(max_behot_time))
 	sinature = firefox.execute_script('return TAC.sign(' + str(max_behot_time) + ')')
 	conn.publish(pubChannel,max_behot_time + ':' + sinature)
