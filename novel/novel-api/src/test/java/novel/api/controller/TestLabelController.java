0730e2539a897a5b0c8aead2b88adfa8cb515293
wangxiaodan <wangxiaodan@sheyuan.com>
2018-07-06 18:13:39 +0800

    添加api项目的controller层单元测试

--- novel/novel-api/src/test/java/novel/api/controller/TestLabelController.java
+++ novel/novel-api/src/test/java/novel/api/controller/TestLabelController.java
@@ -1,32 +1,44 @@
 package novel.api.controller;
 
+import novel.api.App;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Created by wangxiaodan on 2018/7/6.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(classes = App.class)
 public class TestLabelController {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebApplicationContext wac;
+    private MockMvc mvc;
+    private MockHttpSession session;
+
+    @Before
+    public void setupMockMvc() {
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+        session = new MockHttpSession();
+    }
 
     @Test
     public void getTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/label/get?id=1"))
+        mvc.perform(MockMvcRequestBuilders.get("/label/get?id=1"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
-//                .andExpect(MockMvcResultMatchers.content().string("abc"));
 
 
     }
